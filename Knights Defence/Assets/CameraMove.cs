67d172dc78e1864f11c6b06873bc7f46a0921e55
VedowCrain <Calvinvdwesthuizen@gmail.com>
2018-10-12 11:59:54 +0200

    First person controller remake

---
+++ Knights Defence/Assets/CameraMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public Transform rotatePoint;
+    private bool mouselock = true;
+    public float horizontalSpeed = 2.0F;
+    public float verticalSpeed = 2.0F;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mouselock == true)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else if (mouselock == false)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        if (Input.GetButtonDown("Cancel"))
+        {
+            mouselock = !mouselock;
+        }
+
+        float h = horizontalSpeed * Input.GetAxis("Mouse X");
+
+        float v = verticalSpeed * Input.GetAxis("Mouse Y");
+
+        transform.Rotate(v, h, 0);
+    }
+}
---
+++ Knights Defence/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+
+    public Rigidbody playerRb;
+    public float jumpHight;
+    public float gravity;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        translation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        rotation *= Time.deltaTime;
+        transform.Rotate(0, rotation, 0);
+
+        playerRb.AddForce(Vector3.down * gravity);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            playerRb.velocity = new Vector3(0, 0, 0);
+            playerRb.AddForce(Vector3.up * jumpHight, ForceMode.Impulse);
+        }
+    }
+}
