aa1ed05ddfe96f30bb4d03b4eab57056abc260be
VedowCrain <Calvinvdweesthuizen@gmail.com>
2018-10-29 13:26:25 +0200

    Weapon Buttons Created in Coustamize UI

--- Knights Defence/Assets/Scripts/CharaterCustomization.cs
+++ Knights Defence/Assets/Scripts/CharaterCustomization.cs
@@ -5,98 +5,123 @@ using UnityEngine.UI;
 
 public class CharaterCustomization : MonoBehaviour
 {
-    public Button maleButton, femaleButton, finalizeButton;
-    public Transform customizePoint;
-    public Transform spawnPoint;
+	public Button maleButton, femaleButton, finalizeButton;
+	public Transform customizePoint;
+	public Transform spawnPoint;
 
-    private bool male = true;
-    private bool female = false;
-    private bool finalize = false;
+	private bool male = true;
+	private bool female = false;
+	private bool finalize = false;
 
-    public GameObject activeMaleCharaterModles;
-    private GameObject activeMaleCharaterModlesObject;
-    public GameObject activeFemaleCharaterModles;
-    private GameObject activeFemaleCharaterModlesObject;
+	public GameObject activeMaleCharaterModles;
+	private GameObject activeMaleCharaterModlesObject;
+	public GameObject activeFemaleCharaterModles;
+	private GameObject activeFemaleCharaterModlesObject;
 
-    // Use this for initialization
-    void Start()
-    {
-        finalize = false;
 
-        maleButton.onClick.AddListener(MaleGender);
-        femaleButton.onClick.AddListener(FemaleGender);
-        finalizeButton.onClick.AddListener(MoveAvtiveCharaterModle);
+	private CharaterAttack currentCustomizableCharacter;
 
-        MaleGender();
-        MoveAvtiveCharaterModle();
-    }
+	// Use this for initialization
+	void Start()
+	{
+		finalize = false;
 
-    // Update is called once per frame
-    void Update()
-    {
-                
-    }
+		maleButton.onClick.AddListener(MaleGender);
+		femaleButton.onClick.AddListener(FemaleGender);
+		finalizeButton.onClick.AddListener(MoveAvtiveCharaterModle);
 
-    void MaleGender()
-    {
-        male = true;
-        female = false;
+		MaleGender();
+		MoveAvtiveCharaterModle();
+	}
 
-        if (male == true)
-        {
-            Instantiate<GameObject>(activeMaleCharaterModles, customizePoint.position, customizePoint.rotation, customizePoint);
-            activeFemaleCharaterModlesObject = GameObject.FindGameObjectWithTag("Female");
+	// Update is called once per frame
+	void Update()
+	{
 
-            foreach (Transform obj in customizePoint)
-            {
-                Destroy(activeFemaleCharaterModlesObject);
-            }
-        }
-    }
+	}
 
-    void FemaleGender()
-    {
-        male = false;
-        female = true;
+	void MaleGender()
+	{
+		male = true;
+		female = false;
 
-        if (female == true)
-        {
-            Instantiate<GameObject>(activeFemaleCharaterModles, customizePoint.position, customizePoint.rotation, customizePoint);
-            activeMaleCharaterModlesObject = GameObject.FindGameObjectWithTag("Male");
+		if (male == true)
+		{
+			GameObject selectedCharacter = Instantiate(activeMaleCharaterModles, customizePoint.position, customizePoint.rotation, customizePoint);
+			activeFemaleCharaterModlesObject = GameObject.FindGameObjectWithTag("Female");
 
-            foreach (Transform obj in customizePoint)
-            {
-                Destroy(activeMaleCharaterModlesObject);
-            }
-        }
-    }
+			foreach (Transform obj in customizePoint)
+			{
+				Destroy(activeFemaleCharaterModlesObject);
+			}
 
-    void MoveAvtiveCharaterModle()
-    {
-        finalize = true;
+			CharaterAttack attack = selectedCharacter.GetComponent<CharaterAttack>();
+			if (attack != null)
+			{
+				currentCustomizableCharacter = attack;
+			}
+		}
+	}
 
-        if (finalize == true)
-        {
-            if (male == true)
-            {
-                activeMaleCharaterModlesObject = GameObject.FindGameObjectWithTag("Male");
-                activeMaleCharaterModlesObject.transform.position = spawnPoint.transform.position;
-                activeMaleCharaterModlesObject.transform.rotation = spawnPoint.transform.rotation;
-            }
+	void FemaleGender()
+	{
+		male = false;
+		female = true;
 
-            if (female == true)
-            {
-                activeFemaleCharaterModlesObject = GameObject.FindGameObjectWithTag("Female");
-                activeFemaleCharaterModlesObject.transform.position = spawnPoint.transform.position;
-                activeFemaleCharaterModlesObject.transform.rotation = spawnPoint.transform.rotation;
-            }
-            //activeMaleCharaterModlesObject.transform.position = spawnPoint.transform.position;
-            //activeFemaleCharaterModlesObject.transform.position = spawnPoint.transform.position;
-        }
-        else if (finalize == false)
-        {
-            //activeMaleCharaterModlesObject.transform.position = customizePoint.transform.position;
-            //activeFemaleCharaterModlesObject.transform.position = customizePoint.transform.position;
-        }
-    }
+		if (female == true)
+		{
+			Instantiate<GameObject>(activeFemaleCharaterModles, customizePoint.position, customizePoint.rotation, customizePoint);
+			activeMaleCharaterModlesObject = GameObject.FindGameObjectWithTag("Male");
+
+			foreach (Transform obj in customizePoint)
+			{
+				Destroy(activeMaleCharaterModlesObject);
+			}
+		}
+	}
+
+	void MoveAvtiveCharaterModle()
+	{
+		finalize = true;
+
+		if (finalize == true)
+		{
+			if (male == true)
+			{
+				activeMaleCharaterModlesObject = GameObject.FindGameObjectWithTag("Male");
+				activeMaleCharaterModlesObject.transform.position = spawnPoint.transform.position;
+				activeMaleCharaterModlesObject.transform.rotation = spawnPoint.transform.rotation;
+			}
+
+			if (female == true)
+			{
+				activeFemaleCharaterModlesObject = GameObject.FindGameObjectWithTag("Female");
+				activeFemaleCharaterModlesObject.transform.position = spawnPoint.transform.position;
+				activeFemaleCharaterModlesObject.transform.rotation = spawnPoint.transform.rotation;
+			}
+			//activeMaleCharaterModlesObject.transform.position = spawnPoint.transform.position;
+			//activeFemaleCharaterModlesObject.transform.position = spawnPoint.transform.position;
+		}
+		else if (finalize == false)
+		{
+			//activeMaleCharaterModlesObject.transform.position = customizePoint.transform.position;
+			//activeFemaleCharaterModlesObject.transform.position = customizePoint.transform.position;
+		}
+	}
+
+	public void AssignWeaponToCharacter(Weapon weapon)
+	{
+		if (currentCustomizableCharacter != null)
+		{
+			currentCustomizableCharacter.weapon = weapon;
+		}
+	}
+
+	public void AssignMagicToCharacter(Magic magic)
+	{
+		if (currentCustomizableCharacter != null)
+		{
+			currentCustomizableCharacter.magicType = magic;
+		}
+	}
 }
